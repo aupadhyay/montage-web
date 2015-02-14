342aaf7f19741c64ca686c9e0f74f0f8fd836bca
Rayyaan-Mustafa <sonicshadow6@gmail.com>
2015-02-14 14:05:52 -0800

    commit

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <html>
-<<<<<<< HEAD
-	<body>
-		<h1>Hi Rayyaan 1:11 PM</h1>
-=======
 	<head>
 		<title>Montage</title>
 		<link rel="stylesheet" href="css/bootstrap.css">
@@ -16,6 +12,5 @@
 			</div>
 		</div>
 		<script src="js/bootstrap.js"></script>
->>>>>>> 119dced9a4b39a674eddc97ef9ad88b366abbcbb
 	</body>	
 </html>

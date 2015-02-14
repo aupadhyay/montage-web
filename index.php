ed9dbcf2c0b458de461372278d4e982d80470fe8
Yash-Gupta <yash0314@gmail.com>
2015-02-14 15:19:47 -0800

    Fixed Everything

--- index.php
+++ index.php
@@ -3,67 +3,9 @@
 	<head>
 		<title>Montage</title>
 		<link rel="stylesheet" href="css/bootstrap.css">
-		<style>
-			h1{background-color: yellow}
-		</style>
 	</head>
 	<body>
 		<!-- ADD NAV BAR HERE (Here)-->
-<<<<<<< HEAD
-		<nav class="navbar navbar-default" style="background-color:green">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-=======
 		<nav class="navbar navbar-inverse">
   			<div class="container">
    				<div class="navbar-header">
@@ -80,20 +22,17 @@
       				<ul class="nav navbar-nav">
         				<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
         				<li><a href="#">Link</a></li>
-      
+                <li><a href="login-page.php"><button class="btn btn-primary">Login!</button></a></li>
       				</ul>
     			</div><!-- /.navbar-collapse -->
   			</div><!-- /.container-fluid -->
 		</nav>
 		
->>>>>>> FETCH_HEAD
 		<div class="container">
 			<div class="jumbotron">
 				<h1>Hi! Welcome to Montage...</h1>
 			</div>
-      <a href="login-page.php"
-      <button type="primary">Login!</button>
-      </a>
+
       
 
 		</div>
--- login-page.php
+++ login-page.php
@@ -1,2 +1,9 @@
-
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<h1>Login Page</h1>
+</body>
+</html>
 

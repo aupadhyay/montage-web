d4688fe15544307e964d1c85f3e00a5c643d2740
Rayyaan-Mustafa <sonicshadow6@gmail.com>
2015-02-14 14:04:25 -0800

    yash is fat

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
 	<head>
 		<title>Montage</title>
 		<link rel="stylesheet" href="css/bootstrap.css">
+		
 	</head>
 	<body>
-		<h1>Hi!</h1>
+		<h1>Hi, Welcome to Your Friendly Montage Creator!</h1>
+		<div class="jumbotron">
+			<div class="container">
+				<h6> yash is fat</h6>
+			</div>
+		</div>
 		<script src="js/bootstrap.js"></script>
 >>>>>>> 119dced9a4b39a674eddc97ef9ad88b366abbcbb
 	</body>	

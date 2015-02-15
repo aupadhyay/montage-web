c2318f5b7cad8a09fa0679ee43f07c821ebb4c44
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 17:25:41 -0800

    Test

--- login-page.php
+++ login-page.php
@@ -6,7 +6,16 @@
 		
     <div class="container">
 			<div class="jumbotron">
-				<h1>Hi! Welcome to Montage...</h1>
+				<h1>Login</h1>
+				<form method="POST"action="login-sql.php">
+					<label for="email">
+						<input type="email" placeholder="Email...">
+					</label>
+					<label for="password">
+						<input type="password" placeholder="Password...">
+					</label>
+					<input type="submit" class="btn">
+				</form>
 			</div>
 		</div>
 

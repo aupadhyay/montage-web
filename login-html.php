cde9959cb009831820754b4abe21925c6853b095
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 18:30:00 -0800

    Test

--- login-html.php
+++ login-html.php
@@ -3,12 +3,16 @@
 ?>
 <html>
 	<body>
+		<div class="container">
+		<div class="jumbotron">
 		<form action="login.php" method="post">
 			<label for="email">Email:</label>
 			<input type="text" name="email" id="email" placeholder="Email"><br>
 			<label for="password">Password:</label>
 			<input type="password" name="password" id="password" placeholder="Password">
 		</form>
+		</div>
+		</div>
 	</body>
 
 </html>

b528277472d4aab50dfcefc8fb860533a59ce67d
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 16:42:30 -0800

    Test

--- register.php
+++ register.php
@@ -14,19 +14,21 @@
 					<label for="first_name">
 						First Name:
 						<input type="text"class="form-control" id="first_name" name="first_name"placeholder="First Name...">
-					</label>
+					</label><br>
 					<label for="last_name">
 						Last Name:
 						<input type="text"class="form-control" id="last_name" name="last_name"placeholder="Last Name...">
-					</label>
+					</label><br>
 					<label for="email">
 						Email:
 						<input type="email" class="form-control"id="email" name="email"placeholder="Email...">
-					</label>
+					</label><br>
 					<label for="password">
 						Password:
 						<input type="password" class="form-control" id="password" name="password"placeholder="Password...">
-					</label>
+					</label><br>
+
+					<button class="btn"><input type="submit"></button>
 				</form>
 			</div>
 		</div>

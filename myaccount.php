d429ed02f8e610c988428b608819aa666f0e3742
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 22:43:31 -0800

    Test

--- myaccount.php
+++ myaccount.php
@@ -5,7 +5,12 @@
 	$first_name = $_SESSION['first_name'];
 	$last_name = $_SESSION['last_name'];
 	$email = $_SESSION['email'];
-	echo $first_name;
+
+	if($_POST['first_name']){
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$email = $_POST['email'];
+	}
 ?>
 <html>
 	<body>
@@ -15,15 +20,15 @@
 				<form action="myaccount.php" method="post">
 					<label for="first_name">
 						First Name:
-						<input class="form-control" type="text" value=<?php echo $first_name;?>>
+						<input class="form-control" name="first_name" id="first_name" type="text" value=<?php echo $first_name;?>>
 					</label><br>
 					<label for="last_name">
 						Last Name:
-						<input class="form-control" type="text" value=<?php echo $last_name;?>>
+						<input class="form-control" name="last_name" id="last_name" type="text" value=<?php echo $last_name;?>>
 					</label><br>
 					<label for="email">
 						Email:
-						<input class="form-control" type="text" value=<?php echo $email;?>>
+						<input class="form-control" name="email" id="email" type="text" value=<?php echo $email;?>>
 					</label><br>
 					<input class="btn"type="submit">
 				</form>

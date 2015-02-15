a75a12c2b1a4687f07c500193adf4c21bce40838
Yash-Gupta <yash0314@gmail.com>
2015-02-14 18:43:11 -0800

    sanchi is   hawt

--- index.php
+++ index.php
@@ -8,7 +8,13 @@
 			</div>
 
     <h2><strong>What is SparkUs?</strong></h2>
-    <p>SparkUs is a collaborating network for people of all ages who need help <br>in igniting their idea.
+    <p>SparkUs is a collaborating network for people of all ages who need help <br>
+      in igniting their idea. Anyone in need of sponsors or capital can make <br>
+      their own profile. Companies can browse through these profiles and, if <br>
+      they feel that they need it, can contact them to work out a deal. SparkUs<br>
+      is the perfect place for people to interact with other companies in order to<br>
+      gain sponsors. 
+
     </p>
 
 		</div>

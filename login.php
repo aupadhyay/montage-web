f6746766f94cdd0c35723709b3b7b83391083936
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 18:34:45 -0800

    Test

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
 	session_start();
 	require("../databases/config.php");
+	echo "Test";
 	if(isset($_POST['email'])){
 		$username = mysql_escape_string($_POST['email']);
 		$pass = mysql_escape_string($_POST['password']);
 		$query = mysql_query("SELECT * FROM `users` WHERE `email`='$username' AND `pass`='$pass'");
-
+		echo "1";
 		if(mysql_num_rows($query) > 0 ){
 			$_SESSION['loggedin'] = 1;
 			echo "Logged In!";

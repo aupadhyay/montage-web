2c747ace67566fda1ef91a423738a3b1d6271bfb
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 18:19:15 -0800

    Cleanup

---
+++ login-html.php
@@ -0,0 +1,14 @@
+<?php
+	require("header.php");
+?>
+<html>
+	<body>
+		<form action="login.php" method="post">
+			<label for="email">Email:</label>
+			<input type="text" name="email" id="email" placeholder="Email"><br>
+			<label for="password">Password:</label>
+			<input type="password" name="password" id="password" placeholder="Password">
+		</form>
+	</body>
+
+</html>

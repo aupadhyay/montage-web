f379dc45af54196c32c793d8aaa79b628434dcc7
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 17:06:06 -0800

    Test

--- register.php
+++ register.php
@@ -1,5 +1,16 @@
 <?php
 	require("header.php");
+	require("../databases/config.php");
+	if(isset($_POST['first_name'])){
+		echo "Post received!";
+		$first_name = mysql_escape_string($_POST['first_name']);
+		$last_name = mysql_escape_string($_POST['last_name']);
+		$email = mysql_escape_string($_POST['email']); 
+		$password = mysql_escape_string($_POST['password']);
+		$sql_query = mysql_query("INSERT INTO `users` (`first_name`, `last_name`, `email`,`pass`) VALUES ('$first_name','$last_name','$email','$password');");
+		echo "Test";
+	}
+
 ?>
 
 <html>

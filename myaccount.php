21560d0285c95470d38a6ec435c472bbae99d9d1
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 22:32:43 -0800

    Test

--- myaccount.php
+++ myaccount.php
@@ -14,8 +14,15 @@
 				<h1>My Account-<?php echo $first_name;?></h1>
 				<form action="myaccount.php" method="post">
 					<label for="first_name">
-						<input type="text" value=<?php echo $first_name;?>>
+						<input class="form-control" type="text" value=<?php echo $first_name;?>>
 					</label>
+					<label for="last_name">
+						<input class="form-control" type="text" value=<?php echo $last_name;?>>
+					</label>
+					<label for="email">
+						<input class="form-control" type="text" value=<?php echo $email;?>>
+					</label>
+
 				</form>
 			</div>
 		</div>

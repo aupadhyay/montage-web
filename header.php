2cc845305cec6fb7e5a076bb4174376bcdf29c77
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 17:58:17 -0800

    Test

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
       				<a class="navbar-brand" href="#"><img src="img/logo.png" alt="Logo" width="135" height="45"></a>
     			</div>
 
-   				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+   				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
       				<ul class="nav navbar-nav navbar-right">
         				<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
         				<li><a href="#">Link</a></li>

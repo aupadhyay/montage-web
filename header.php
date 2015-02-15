dcc2f744147820a7b71e3fcb2e6858c08a7e7052
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 17:47:58 -0800

    Test

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
         				<span class="icon-bar"></span>
         				<span class="icon-bar"></span>
       				</button>
-      				<a class="navbar-brand" href="#">Brand</a>
+      				<a class="navbar-brand" href="#"><img src="img/logo.png" alt="Logo"></a>
     			</div>
 
    				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">

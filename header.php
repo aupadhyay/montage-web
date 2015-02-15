527e2aac0274db738403e10bef5c6f2a428dc212
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 21:24:45 -0800

    Test

--- header.php
+++ header.php
@@ -21,10 +21,10 @@
 
    				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
       				<ul class="nav navbar-nav">
-        				<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-        				<li><a href="#">About</a></li>
-                <li><a href="login-page.php"><button class="btn btn-primary">Login!</button></a></li>
-                <li><a href="register.php"><button class="btn btn-primary">Register!</button></a></li>
+        				<li class="active nav-link"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        				<li class="nav-link"><a href="#">About</a></li>
+                <li class="nav-link"><a href="login-page.php"><button class="btn btn-primary">Login!</button></a></li>
+                <li class="nav-link"><a href="register.php"><button class="btn btn-primary">Register!</button></a></li>
       				</ul>
     			</div><!-- /.navbar-collapse -->
   			</div><!-- /.container-fluid -->

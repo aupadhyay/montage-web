f93f29e472032454c0a6b8a0963b4ec975b9dfef
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 21:34:29 -0800

    Test

--- header.php
+++ header.php
@@ -23,8 +23,8 @@
       				<ul class="nav navbar-nav">
         				<li class="nav-link"><a href="#">Home <span class="sr-only">(current)</span></a></li>
         				<li class="nav-link"><a href="#" style="color: #def2ff !important;">About</a></li>
-                <li class="nav-link"><a href="login-page.php"><button class="btn btn-primary">Login!</button></a></li>
-                <li class="nav-link"><a href="register.php"><button class="btn btn-primary">Register!</button></a></li>
+                <li class="nav-link"><a href="login-page.php">Login!</a></li>
+                <li class="nav-link"><a href="register.php">Register!</a></li>
       				</ul>
     			</div><!-- /.navbar-collapse -->
   			</div><!-- /.container-fluid -->

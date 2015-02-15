fbfbb28858da7038a5a7670c0dfede972c74da2b
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-14 17:16:53 -0800

    Test

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
 		$last_name = mysql_escape_string($_POST['last_name']);
 		$email = mysql_escape_string($_POST['email']); 
 		$password = mysql_escape_string($_POST['password']);
-		$sql_query = mysql_query("INSERT INTO `users` (`first_name`, `last_name`, `email`,`pass`) VALUES ('$first_name','$last_name','$email','$password');");
+		$sql_query = mysql_query("INSERT INTO `users`(`first_name`, `last_name`, `email`,`pass`) VALUES ('$first_name','$last_name','$email','$password');");
 		echo $sql_query;
 		echo $first_name;
 		echo $last_name;
